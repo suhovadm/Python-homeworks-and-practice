b350dd20a72deb1d5620ed3ac31106b7a9da6291
n1rvana <163005667+suhovadm@users.noreply.github.com>
2025-05-20 16:06:51 +0500

    Add files via upload

---
+++ Geometric figures/11.02.2024_Пустой и заполненный квадраты через функцию без параметров/main.py
@@ -0,0 +1,39 @@
+import empty_square
+import filled_square
+
+def main():
+    while(True):
+        try:
+
+            # Меню с выбором типа квадрата.
+            print('[   Меню   ]')
+            print('1. Пустой квадрат.')  # value 1.
+            print('2. Заполненный квадрат.')  # value 2.
+            print('3. Выход.') # value 3.
+
+            # Приглашение к выбору типа квадрата.
+            choice = int(input('Выберите тип квадрата (1, или 2):~$ '))
+
+            # 1. Пустой квадрат.
+            if (choice == 1):
+                empty_square.kvadratS()
+
+            # 2. Заполненный квадрат.
+            elif (choice == 2):
+                filled_square.kvadratS()
+
+            # 3. Выход из программы.
+            elif (choice == 3):
+                print('\nДо новых встреч.')
+                break
+
+            else:
+                print('\nОшибка! Пожалуйста, выберите пункт 1 или 2.\n')
+
+        except ValueError:
+            print('\nОшибка! Похоже, что вы ввели буквы или спец. символы.')
+            print('Пожалуйста, выберите пункт 1 или 2.\n')
+            continue
+
+if __name__ == '__main__':
+    main()
